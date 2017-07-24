a38ae3c6573ca5d6d4f120459984a1238b5ebdf6
ainich <lx.nich@gmail.com>
2017-07-24 12:33:35 +0300

    modified:   iocwatch.py

--- iocwatch.py
+++ iocwatch.py
@@ -32,28 +32,55 @@ class CONNStats(Model):
     
     engine = MergeTree('event_date', ('timestamp', 'protocol', 'src_addr', 'src_port', 'dst_addr', 'dst_port', 'qry_name'))
 
-db = Database('conn_stat')
+class IOCStats(Model):
+
+    event_date = DateField()
+    timestamp = DateTimeField()
+    protocol = StringField()
+    src_addr = StringField()
+    src_port = Float32Field()
+    dst_addr = StringField()
+    dst_port = Float32Field()
+    qry_name = StringField()
+    indicator = StringField()
+    name = StringField()
+    references = StringField()
+
+    engine = MergeTree('event_date', ('timestamp', 'protocol', 'src_addr', 'src_port', 'dst_addr', 'dst_port', 'qry_name', 'indicator', 'name', 'references'))
+
+#db = Database('conn_stat')
 #db.create_table(CONNStats)
 
 
-#db = Database('comromised')
-#db.create_table(IOC_OTX)
+db = Database('ioc')
+db.create_table(IOCStats)
 
 # 5 min time
 tz = timezone('Europe/Moscow')
 to_time = datetime.datetime.now()
 from_time = (datetime.datetime.now(tz) - datetime.timedelta(minutes=5)).replace(microsecond=0)
-timestamp = str(from_time.timestamp())
+from_time_epoch = str(from_time.timestamp())
+today = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
 
 class OTXWatch():
 
     def get_traf_last(self):
             print ("Starting fetch traffic stat ...")
-            for row in db.select('SELECT timestamp, src_addr, src_port, dst_addr, dst_port, qry_name FROM conn_stat.connstats WHERE timestamp >= toDateTime('+timestamp+') ORDER BY timestamp'):
+            for row in db.select('SELECT * FROM conn_stat.connstats WHERE timestamp >= toDateTime('+from_time_epoch+') ORDER BY timestamp'):
                 #print (row.timestamp, row.src_addr, row.dst_addr, type(row.dst_addr))
-                for ioc in db.select('SELECT * FROM ioc.ioc_otx WHERE indicator = \''+row.dst_addr+'\' ORDER BY timestamp'):
-                    print (ioc.name)
-            
+                timestamp = datetime.datetime.now()
+                if not row.qry_name == 'none':
+                    for ioc in db.select('SELECT * FROM ioc.ioc_otx WHERE indicator = \''+row.qry_name+'\' ORDER BY timestamp'):
+                        print (row.src_addr, row.src_port, row.dst_addr, row.dst_port, row.qry_name, ioc.name, ioc.indicator, ioc.references)
+                        db.insert([
+                        IOCStats(event_date=today, timestamp=timestamp, protocol=row.protocol, src_addr=row.src_addr, src_port=row.src_port, dst_addr=row.dst_addr, dst_port=row.dst_port, qry_name=row.qry_name, indicator=ioc.indicator, name=ioc.name, references=ioc.references)
+                        ])    
+                else:
+                    for ioc in db.select('SELECT * FROM ioc.ioc_otx WHERE indicator = \''+row.dst_addr+'\' ORDER BY timestamp'):
+                        print (row.src_addr, row.src_port, row.dst_addr, row.dst_port, row.qry_name, ioc.name, ioc.indicator, ioc.references)
+                        db.insert([
+                        IOCStats(event_date=today, timestamp=timestamp, protocol=row.protocol, src_addr=row.src_addr, src_port=row.src_port, dst_addr=row.dst_addr, dst_port=row.dst_port, qry_name=row.qry_name, indicator=ioc.indicator, name=ioc.name, references=ioc.references)
+                        ])    
 
 #    def check_iocs(self):
 #
