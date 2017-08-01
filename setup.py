4517dad5278b4358318e6158dac394e272ec47f6
ainich <lx.nich@gmail.com>
2017-08-01 11:44:22 +0300

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@
 ##############################################################################
 
 import dbmodels
+import shutil
+import os
 import yaml
 
 # Read config
@@ -17,9 +19,26 @@ with open("/etc/politraf/config.yaml", 'r') as stream:
     except yaml.YAMLError as e:
         print(e)
 
-db = dbmodels.Database('politraf', db_url=url, username=name, password=passw, readonly=False, autocreate=True)
-db.create_table(CONNStats)
-db.create_table(CPUStats)
-db.create_table(MEMStats)
-db.create_table(DISKStats)
-db.create_table(IOCStats)
+
+try:
+    db = dbmodels.Database('politraf', db_url=url, username=name, password=passw, readonly=False, autocreate=True)
+    print ("Make dir /opt/politraf")
+    os.makedirs("/opt/politraf1")
+    print ("Copy config.yaml to /etc/politraf")
+    shutil.copy2('config/config.yaml', '/etc/politraf/config.yaml1')
+    print ("Setup services")
+    shutil.copy2('config/systat.service', '/etc/systemd/system/systat.service1')
+    shutil.copy2('config/constat.service', '/etc/systemd/system/constat.service1')
+    print ("Copy politraf files")
+    shutil.copy2('systat.py', '/opt/politraf1/systat.py1')
+    shutil.copy2('otxget.py', '/opt/politraf1/otxget.py1')
+    shutil.copy2('constat.py', '/opt/politraf1/constat.py1')
+    shutil.copy2('dbmodels.py', '/opt/politraf1/dbmodels.py1')
+    print ("Create database with tables")
+    db.create_table(dbmodels.CONNStats)
+    db.create_table(dbmodels.CPUStats)
+    db.create_table(dbmodels.MEMStats)
+    db.create_table(dbmodels.DISKStats)
+    db.create_table(dbmodels.IOCStats)
+except Exception as e:
+    print(e)
