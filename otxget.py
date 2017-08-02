0feb9f9e38cc293193bf31c7c7ccb17e6dea13ef
ainich <lx.nich@gmail.com>
2017-08-02 17:22:14 +0300

    modified:   .travis.yml 	modified:   constat.py 	modified:   iocwatch.py 	modified:   otxget.py 	modified:   setup.py 	modified:   systat.py

--- otxget.py
+++ otxget.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 ##############################################################################
 # Politraf, IOC from OTX to clickhouse
