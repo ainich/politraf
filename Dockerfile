b1136d8d39f16443dc3e489b2b41bafc6222bef9
ainich <lx.nich@gmail.com>
2018-07-10 09:36:25 +0300

    modified:   Dockerfile 	new file:   install_for_docker.py

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM debian:8
 MAINTAINER lx.nich@gmail.com
 
-RUN apt-get update && apt-get install -y openssh-server apt-transport-https nginx git curl
+RUN apt-get update && apt-get install -y openssh-server apt-transport-https nginx git curl python3 python3-pip libxml2-dev libxslt1-dev python-dev python3-dev python3-lxml
 RUN echo 'deb http://repo.yandex.ru/clickhouse/deb/stable/ main/' | tee -a /etc/apt/sources.list
 RUN  apt-key adv --keyserver keyserver.ubuntu.com --recv E0C56BD4
 RUN echo 'deb https://packagecloud.io/grafana/stable/debian/ jessie main' | tee -a /etc/apt/sources.list
 RUN curl https://packagecloud.io/gpg.key | apt-key add -
-RUN git clone git@gitlab.com:ainich/politraf.git
-
+RUN mkdir -p ~/.ssh
+RUN git clone https://gitlab.com/ainich/politraf.git
+RUN pip3 install -r /politraf/requirements.txt
+RUN python3 /politraf/install_for_docker.py
 
 EXPOSE 22 80
 
---
+++ install_for_docker.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+# -*- coding: utf-8
+##############################################################################
+# Politraf, initial setup
+##############################################################################
+
+from subprocess import call
+from subprocess import Popen
+import shutil
+import os
+import time
+
+
+green='\033[32m'
+greene='\033[0m'
+orange='\033[33m'
+orangee='\033[0m'
+blue='\033[34m'
+
+try:
+    print (green + "Install requirements" + greene)
+    call(["apt-get", "install" , "tshark"])
+    os.putenv("LC_ALL", "C")
+    #Popen('export LC_ALL=C', shell=True, executable='/bin/bash')
+    call(["apt-get", "install" , "python3-pip"])
+    call(["pip3", "install" , "-r" , "requirements.txt"])
+    call(["pip3", "install" , "--upgrade", "six"])
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+
+import dbmodels
+
+try:
+    print (green + 'Install Clickhouse and Grafana' + greene)
+    call(["apt-get", "update"])
+    call(["apt-get", "install" , "grafana"])
+    call(["apt-get", "install" , "clickhouse-client" , "clickhouse-server-common"])
+    call(["grafana-cli", "plugins" , "install" , "vertamedia-clickhouse-datasource"])
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+
+try:
+    print (green + "Start Clickhouse and Grafana" + greene)
+    call(["service", "clickhouse-server" , "restart"])
+    call(["service", "grafana-server" , "restart"])
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+
+try:
+    print (green +"Make dir /etc/politraf" + greene)
+    os.makedirs("/etc/politraf")
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+try:
+    print (green +"Make dir /opt/politraf/current" + greene)
+    os.makedirs("/opt/politraf/current")
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+try:
+    print (green +"Copy config.yaml to /etc/politraf" + greene)
+    shutil.copy2('/politraf/config/config.yaml', '/etc/politraf/config.yaml')
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+try:
+    print (green + 'Make dir /opt/politraf' + greene)
+    os.makedirs("/opt/politraf")
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+try:
+    print (green +'Setup services' + greene)
+    shutil.copy2('/politraf/config/systat.service', '/etc/systemd/system/systat.service')
+    shutil.copy2('/politraf/config/constat.service', '/etc/systemd/system/constat.service')
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+try:
+    print (green + 'Copy politraf files' + greene)
+    shutil.copy2('/politraf/src/systat.py', '/opt/politraf/systat.py')
+    shutil.copy2('/politraf/src/otxget.py', '/opt/politraf/otxget.py')
+    shutil.copy2('/politraf/src/constat.py', '/opt/politraf/constat.py')
+    shutil.copy2('/politraf/src/csv_load_to_db.py', '/opt/politraf/csv_load_to_db.py')
+    shutil.copy2('/politraf/src/ext_cscan.py', '/opt/politraf/ext_cscan.py')
+    shutil.copy2('/politraf/src/iocwatch.py', '/opt/politraf/iocwatch.py')
+    shutil.copy2('/politraf/dbmodels.py', '/opt/politraf/dbmodels.py')
+    shutil.copy2('/politraf/src/ioc_self_get.py', '/opt/politraf/ioc_self_get.py')
+    shutil.copy2('/politraf/src/ioc_self_watch.py', '/opt/politraf/ioc_self_watch.py')
+    shutil.copy2('/politraf/src/self_ioc_list.csv', '/opt/politraf/self_ioc_list.csv')
+    #os.chmod("src/systat.py", stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+    #os.chmod("src/otxget.py", stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+    #os.chmod("src/constat.py", stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+    #os.chmod("src/dbmodels.py", stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
+try:
+    print (green + 'Wait for clickhouse init and create database with tables' + greene)
+    time.sleep(20)
+    # Create tables
+    db = dbmodels.Database('politraf', db_url="http://127.0.0.1:8123/", username="default", password="", readonly=False, autocreate=True)
+    db.create_table(dbmodels.CONNStats)
+    db.create_table(dbmodels.CONNStats_buffer)
+    db.create_table(dbmodels.CPUStats)
+    db.create_table(dbmodels.MEMStats)
+    db.create_table(dbmodels.DISKStats)
+    db.create_table(dbmodels.IOCStats)
+    db.create_table(dbmodels.OPEN_PORTS)
+    print (green + 'Done' + greene)
+except Exception as e:
+    print(orange, e, orangee)
